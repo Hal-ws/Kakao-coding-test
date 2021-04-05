3c218351d293d26eefd1afe9c98e11a8451a8dca
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-04-05 14:45:55 +0900

    Update 42891.py

--- 42891.py
+++ 42891.py
@@ -1,5 +1,7 @@
 def solution(food_times, k):
     left, right = min(food_times), max(food_times)
+    if sum(food_times) <= k:
+        return -1
     minEat = right + 1
     l = len(food_times)
     while left <= right:
@@ -21,25 +23,20 @@ def solution(food_times, k):
                 minEat = eat
         else: # 다 못먹음
             left = eat + 1
-    if sum(food_times) <= k:
-        return -1
-    else:
-        totalTime = 0
-        for i in range(l):
-            time = food_times[i]
-            if time >= minEat:
-                totalTime += minEat
-            else:
-                totalTime += time
-            if totalTime == k:
-                start = i
-                break
-            if totalTime > k:
-                return i + 1
-        for i in range(start + 1, l):
-            if food_times[i] >= minEat: # 다음으로 먹을 수 있는거 발견
-                return i + 1
-        minEat += 1
-        for i in range(l):
-            if food_times[i] >= minEat:
-                return i + 1
+    totalTime = 0
+    for i in range(l):
+        time = food_times[i]
+        if time >= minEat - 1:
+            totalTime += minEat - 1
+        else:
+            totalTime += time
+    for i in range(l):
+        time = food_times[i]
+        if time >= minEat:
+            totalTime += 1
+        if totalTime > k: # i번 idx의 음식을 먹어야 함
+            return i + 1
+    minEat += 1
+    for i in range(l):
+        if food_times[i] >= minEat:
+            return i + 1
