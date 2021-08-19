9a4b0b91fc4972abb59fd11fc76c434ba0e5cc36
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-08-19 22:42:23 +0900

    Update 72414.py

--- 72414.py
+++ 72414.py
@@ -1,3 +1,19 @@
 def solution(play_time, adv_time, logs):
     answer = ''
+    play_time = time_to_sec(play_time)
+    adv_time = time_to_sec(adv_time)
+    timetable = [0 for i in range(play_time + 1)]
+    for log in logs:
+        
+    
+
     return answer
+
+
+def time_to_sec(time):
+    h, m, s = map(int, time.split(':'))
+    return h * 3600 + m * 60 + s
+
+
+def sec_to_time(sec):
+    return ''
